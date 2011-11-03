33b21e8ac94abb2e582d621a9398bc8659fb495f
Brian Pfeil <brian.pfeil@gmail.com>
2011-11-03 13:34:38 -0400

    first iteration of search-as-you-type

--- informatica-cloud-web-app-bookmarklet.js
+++ informatica-cloud-web-app-bookmarklet.js
@@ -1,4 +1,23 @@
 (function() {
+    
+    var searchInputElement = document.getElementById('taskName');
+    searchInputElement.addEventListener('keydown', function() {
+        var query = searchInputElement.value;
+        var rows = document.querySelectorAll('table.InfPane div.InfList div.body table tbody tr');
+        console.log('query = ' + query);
+        for (var i = 1; i < rows.length; i++) {
+            var row = rows[i];
+            
+            var regex = new RegExp(query, 'igm')
+            if ( row.textContent.match(regex) ) {
+                row.style.display = '';
+            } else {
+                row.style.display = 'none';
+            }
+        }
+        
+    }, false);
+    
     var sel = document.getElementById('InfPageSz');
     if (sel == null) {
         return;
