35fd4215cb4785dcc912dda8026bef09d37915a7
Brian Pfeil <brian.pfeil@gmail.com>
2011-11-03 12:53:25 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Informatica Cloud Web App Bookmarklet #
+
+A bookmarklet to enhance the Informatica Cloud Web App UI and Functionality
---
+++ bookmarklet.js
@@ -0,0 +1,16 @@
+javascript: function icwab1() {
+    var d = document,
+    z = d.createElement('scr' + 'ipt'),
+    b = d.body,
+    l = d.location;
+    try {
+        if (!b) throw (0);
+        d.title = '(Loading...) ' + d.title;
+        z.setAttribute('src', l.protocol + '//www.github.com/j/WvwBY1HBslrt?u=' + encodeURIComponent(l.href) + '&t=' + (new Date().getTime()));
+        b.appendChild(z);
+    } catch(e) {
+        alert('Please wait until the page has loaded.');
+    }
+}
+icwab1();
+void(0)
