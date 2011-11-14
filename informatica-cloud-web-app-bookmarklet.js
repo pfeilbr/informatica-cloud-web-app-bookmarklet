cdcc21d025fce5712aab6c9a60833cc9b1a5f6a9
Brian Pfeil <brian.pfeil@gmail.com>
2011-11-14 11:39:10 -0500

    added element exist checks before trying to add enhancement

--- informatica-cloud-web-app-bookmarklet.js
+++ informatica-cloud-web-app-bookmarklet.js
@@ -1,29 +1,31 @@
 (function() {
     
     var searchInputElement = document.getElementById('taskName');
-    searchInputElement.addEventListener('keydown', function() {
-        var query = searchInputElement.value;
-        var rows = document.querySelectorAll('table.InfPane div.InfList div.body table tbody tr');
-        console.log('query = ' + query);
-        for (var i = 1; i < rows.length; i++) {
-            var row = rows[i];
-            
-            var regex = new RegExp(query, 'igm')
-            if ( row.textContent.match(regex) ) {
-                row.style.display = '';
-            } else {
-                row.style.display = 'none';
+    
+    if (searchInputElement) {
+        searchInputElement.addEventListener('keydown', function() {
+            var query = searchInputElement.value;
+            var rows = document.querySelectorAll('table.InfPane div.InfList div.body table tbody tr');
+            console.log('query = ' + query);
+            for (var i = 1; i < rows.length; i++) {
+                var row = rows[i];
+
+                var regex = new RegExp(query, 'igm')
+                if ( row.textContent.match(regex) ) {
+                    row.style.display = '';
+                } else {
+                    row.style.display = 'none';
+                }
             }
-        }
-        
-    }, false);
+
+        }, false);        
+    }
     
     var sel = document.getElementById('InfPageSz');
-    if (sel == null) {
-        return;
+    if (sel) {
+        var opt = document.createElement('option');
+        opt.setAttribute('value', '999');
+        opt.appendChild(document.createTextNode('999'));
+        sel.appendChild(opt);
     }
-    var opt = document.createElement('option');
-    opt.setAttribute('value', '999');
-    opt.appendChild(document.createTextNode('999'));
-    sel.appendChild(opt);
 })();
